659195044905e9010da667945c0eb91f7f5bf28f
oetawan_c <oetawan_c@MTPC9025>
2016-10-06 17:28:12 +0700

    Test customer

--- eDirectory.Test/CustomerTests.cs
+++ eDirectory.Test/CustomerTests.cs
@@ -22,7 +22,7 @@ namespace eDirectory.Test
 
             var customer = Customer.Create(repository, dto);
             Assert.IsFalse(customer.Id == 0, "Customer Id should have been updated");
-            Assert.AreSame(customer.FirstName, dto.FirstName, "First Name are differenct");
+            Assert.AreSame(customer.FirstName, "Joe", "First Name are differenct");
         }
     }
 }
